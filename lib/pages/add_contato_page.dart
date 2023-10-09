fbda1d8e5499bbaad728840aeb318877de48c6bb
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-10-09 19:21:01 -0300

    add tela para incluir novo contato

---
+++ lib/pages/add_contato_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddContatoPage extends StatefulWidget {
+  @override
+  _AddContatoPageState createState() => _AddContatoPageState();
+}
+
+class _AddContatoPageState extends State<AddContatoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Novo Contato')),
+        body: Container());
+  }
+}
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,7 @@
+import 'package:app_contatos/pages/add_contato_page.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -8,6 +11,17 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Lista de Contatos - Back4app')),
+      floatingActionButton: FloatingActionButton(
+        child: const FaIcon(FontAwesomeIcons.userPlus),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddContatoPage()),
+          );
+        },
+      ),
+    );
   }
 }
